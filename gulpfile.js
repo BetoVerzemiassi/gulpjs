16227ce4080550babda5f31d6720eb7c5a3d4dc8
BetoVerzemiassi <betoverzemiassi@gmail.com>
2017-09-08 23:26:19 -0300

    Aula 02 Integridade do projeto, funcionamento do gulp-clean

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,31 @@
 var gulp = require('gulp'),
-    imagemin = require('gulp-imagemin');
+    imagemin = require('gulp-imagemin'),
+    clean = require('gulp-clean');
 
-gulp.task('build-img', function(){
+gulp.task('copy', ['clean'], function(){
+    return gulp.src('src/**/*')
+        .pipe(gulp.dest('dist'));    
+});
+
+gulp.task('clean', function(){
+    return gulp.src('dist')
+        .pipe(clean());       
+});
+
+gulp.task('build-img', ['copy'], function(){
     gulp.src('src/img/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('src/img'))
 });
+
+/*
+Poderíamos escrever assim o retorno do stream
+
+gulp.task('clean', function() {
+    var stream = gulp.src('dist')
+        .pipe(clean());
+    return stream;
+});
+*/
 
 
